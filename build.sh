51f18b9c0a18bc8b8f8d58cd943a60c6f0ee1a49
yannik@MBP <jenkins@cyanogenmod.com>
2013-04-12 16:27:41 +0200

    cleans every 6 days instead of 1

--- build.sh
+++ build.sh
@@ -248,7 +248,7 @@ fi
 TIME_SINCE_LAST_CLEAN=$(expr $(date +%s) - $LAST_CLEAN)
 # convert this to hours
 TIME_SINCE_LAST_CLEAN=$(expr $TIME_SINCE_LAST_CLEAN / 60 / 60)
-if [ $TIME_SINCE_LAST_CLEAN -gt "24" -o $CLEAN = "true" ]
+if [ $TIME_SINCE_LAST_CLEAN -gt "144" -o $CLEAN = "true" ]
 then
   echo "Cleaning!"
   touch .clean
