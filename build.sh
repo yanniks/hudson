1b604204c818ef45e16b763bf44e6c3e8870946e
yanniks <kontakt@yanniks.de>
2013-07-25 14:43:43 +0200

    some small modifications

--- build.sh
+++ build.sh
@@ -43,6 +43,9 @@ then
   elif [[ "$ROM" =~ "cm" || $ROM =~ "CyanogenMod" ]]; then
    export REPO_BRANCH=cm-10.1
    export FOLDERPREFIX=cm
+  elif [[ "$ROM" =~ "cmworkaround" || $ROM =~ "cmworkaround" ]]; then
+   export REPO_BRANCH=cm-10.2
+   export FOLDERPREFIX=cm
   else
    export REPO_BRANCH=cm-10.1
    export FOLDERPREFIX=cm
@@ -125,10 +128,10 @@ elif [[ "$ROM" =~ "liquid" || $ROM =~ "LiquidSmooth" ]]; then
 elif [[ "$ROM" =~ "slim" || $ROM =~ "SlimBean" ]]; then
    export ROMPROJECT=SlimRoms
    export MANIFESTNAME=platform_manifest
-elif [[ "$ROM" =~ "cm" || $ROM =~ "CyanogenMod" ]]; then
+elif [[ "$ROM" =~ "CyanogenMod" || $ROM =~ "CyanogenMod" ]]; then
    export ROMPROJECT=CyanogenMod
    export MANIFESTNAME=android
-elif [[ "$ROM" =~ "cm" || $ROM =~ "cmworkaround" ]]; then
+elif [[ "$ROM" =~ "cmworkaround" || $ROM =~ "cmworkaround" ]]; then
    export ROMPROJECT=yanniks
    export MANIFESTNAME=android
 else
